4425a41772e6764e70aa52691f3915d403d30245
hitakarento <hitakarento@gmail.com>
2020-02-18 22:59:26 +0900

    TIL

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -5,8 +5,12 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.login.domain.model.SignupForm;
 
 @Controller
 public class SignupController {
@@ -20,15 +24,14 @@ public class SignupController {
   }
 
   @GetMapping("/signup")
-  public String getSignup(Model model) {
+  public String getSignup(@ModelAttribute SignupForm form, Model model) {
     radioMarriage = initRadioMarriage();
     model.addAttribute("radioMarriage", radioMarriage);
     return "login/signup";
   }
-  
+
   @PostMapping("/signup")
-  public String postSignUp(Model model) {
-    System.out.println("test");
+  public String postSignUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
     return "redirect:/login";
   }
 
