1b614103f1edab6e8ffa90f19ecb94fba6eed079
hitakarento <hitakarento@gmail.com>
2020-02-22 12:31:08 +0900

    Signup画面のバリデーション追加

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.GroupOrder;
 import com.example.demo.login.domain.model.SignupForm;
 
 @Controller
@@ -27,13 +29,15 @@ public class SignupController {
   public String getSignup(@ModelAttribute SignupForm form, Model model) {
     radioMarriage = initRadioMarriage();
     model.addAttribute("radioMarriage", radioMarriage);
-    System.out.println("test");
-    System.out.println("test");
     return "login/signup";
   }
 
   @PostMapping("/signup")
-  public String postSignUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
+  public String postSignUp(@ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult, Model model) {
+    if (bindingResult.hasErrors()) {
+      return getSignup(form, model);
+    }
+    System.out.println(form);
     return "redirect:/login";
   }
 
